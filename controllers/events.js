ddf2430dab506f84a46a5988250daab57e2d3d13
SmitCoderX <anirudh16june@gmail.com>
2023-12-02 01:58:47 +0530

    Added Error & Custom Async Middlewares

--- controllers/events.js
+++ controllers/events.js
@@ -1,105 +1,82 @@
 const Events = require('../models/Events');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 //  @desc   Get All Events
 //  @route  GET /api/v1/events
 // @access  Public
-exports.getEvents = async (req, res, next) => {
-  try {
-    const events = await Events.find();
-    res.status(200).json({
-      success: true,
-      count: events.length,
-      data: events,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    });
-  }
-};
+exports.getEvents = asyncHandler(async (req, res, next) => {
+  const events = await Events.find();
+  res.status(200).json({
+    success: true,
+    count: events.length,
+    data: events,
+  });
+});
 
 //  @desc   Get Single Events
 //  @route  GET /api/v1/events/:id
 // @access  Public
-exports.getEvent = async (req, res, next) => {
-  try {
-    const event = await Events.findById(req.params.id);
+exports.getEvent = asyncHandler(async (req, res, next) => {
+  const event = await Events.findById(req.params.id);
 
-    if (!event) {
-      return next(
-        new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: event,
-    });
-  } catch (error) {
-    next(new ErrorResponse(`Event not found with id of ${req.params.id}`, 404));
+  if (!event) {
+    return next(
+      new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: event,
+  });
+});
 
 //  @desc   Create New Events
 //  @route  POST /api/v1/events/
 // @access  Private
-exports.createEvent = async (req, res, next) => {
-  try {
-    const create = await Events.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: create,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    });
-  }
-};
+exports.createEvent = asyncHandler(async (req, res, next) => {
+  const create = await Events.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: create,
+  });
+});
 
 //  @desc   Update Event
 //  @route  PUT /api/v1/events/:id
 // @access  Private
-exports.updateEvent = async (req, res, next) => {
-  try {
-    const event = await Events.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!event) {
-      return next(
-        new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: event,
-    });
-  } catch (error) {
-    next(new ErrorResponse(`Event not found with id of ${req.params.id}`, 404));
+exports.updateEvent = asyncHandler(async (req, res, next) => {
+  const event = await Events.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!event) {
+    return next(
+      new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: event,
+  });
+});
 
 //  @desc   Delete Event
 //  @route  DELETE /api/v1/events/:id
 // @access  Public
-exports.deleteEvent = async (req, res, next) => {
-  try {
-    const event = await Events.findByIdAndDelete(req.params.id);
+exports.deleteEvent = asyncHandler(async (req, res, next) => {
+  const event = await Events.findByIdAndDelete(req.params.id);
 
-    if (!event) {
-      return next(
-        new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: [],
-    });
-  } catch (error) {
-    next(new ErrorResponse(`Event not found with id of ${req.params.id}`, 404));
+  if (!event) {
+    return next(
+      new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: [],
+  });
+});
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,33 @@
-const errorHandler = (err, req, res, next) => {
-  // Log to Console for dev
-  console.log(err.stack.red);
+const ErrorResponse = require('../utils/errorResponse');
 
-  res.status(err.statusCode || 500).json({
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  // Log to Console for dev
+  console.log(err);
+
+  // Mongoose Bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Event not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = 'Duplicate Field Value Entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'Server Error',
+    error: error.message || 'Server Error',
   });
 };
 
